4371848284de3e15c29685838e47774b94fcdeeb
Kartikey Tanna <tannakartikey@gmail.com>
2017-11-18 22:00:59 +0530

    Added routes in the app

---
+++ src/components/router/Link.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+
+export class Link extends Component {
+  handleClick = (e) => {
+    e.preventDefault()
+    window.history.pushState(null, '', this.props.to)
+  }
+  render() {
+    return <a href="#"
+    onClick={this.handleClick}>
+      {this.props.children} 
+    </a>
+  }
+}
+
+Link.PropTypes = {
+  to: PropTypes.string.isRequired
+}
